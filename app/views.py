6989d1dc8abbc3d69c5596464ce62f66d59664b4
Hyounggyu Kim <code@hyounggyu.com>
2020-12-30 15:41:37 +0900

    Change directory structure

--- app/views.py
+++ app/views.py
@@ -5,19 +5,11 @@ from pathlib import Path
 import re
 import shutil
 
-from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash, jsonify, send_from_directory
-from werkzeug import secure_filename
+from flask import request, session, g, redirect, url_for, abort, render_template, flash, jsonify, send_from_directory
+from werkzeug.utils import secure_filename
 from werkzeug.datastructures import ImmutableMultiDict
 
-app = Flask(__name__)
-
-app.config.update(
-    DEBUG = True,
-    TESTING = True,
-    SECRET_KEY = 'development',
-    UPLOAD_FOLDER = './uploads',
-    MAX_CONTENT_PATH = 10485760, # 10MB
-)
+from app import app
 
 @app.route('/')
 def index():
